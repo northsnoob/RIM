647ab7dd07974054849eafe85289ed6066df384c
northsnoob <122870075+northsnoob@users.noreply.github.com>
2025-02-07 02:34:57 +0800

    verification pass

--- RIM.v
+++ RIM.v
@@ -8,32 +8,49 @@ reg [79:0] map;
 reg [3:0] count_rat;
 reg [2:0] count_maze;
 reg [2:0] col,row;
-reg [2:0] ans_col,ans_row[0:15];
+reg [2:0] ans_col [0:14];
+reg [2:0] ans_row [0:14];
 reg [3:0] path_num;
+wire [3:0] stack_path_num;
+wire [2:0] stack_row,stack_col;
+wire down_right;
+wire [6:0] project_p,project_p_d,project_p_r;
+
 assign project_p = (row<<3)+row+col;
 assign project_p_d = project_p+9;
 assign project_p_r = project_p+1;
+wire dead_path;
 /************* input maze *************/
-assign map_h = {(count_maze+1),3'b000}-1+count_maze;
-assign map_l = {(count_maze),3'b000}+count_maze;
+wire [6:0] map_l;
+// assign map_h = {(count_maze+1),3'b000}-1+count_maze;
+assign map_l = {count_maze,count_maze};
+wire complete_ans;
 assign complete_ans = (path_num==15 && count_rat!=15)? 1'b1:1'b0;
 always@(posedge clk or negedge rst_n)begin
     if(!rst_n)begin
-        count_maze<=3'd0;
-        col<=3'd0;
-        row<=3'd0;
+        count_maze <= 3'd0;
+        col <= 3'd0;
+        row <= 3'd0;
         map <= 80'd0;
         path_num <= 0;
     end else begin
         if(in_valid)begin
-            map[map_h:map_l] <= maze;
+            map[map_l]   <= maze[0];
+            map[map_l+1] <= maze[1];
+            map[map_l+2] <= maze[2];
+            map[map_l+3] <= maze[3];
+            map[map_l+4] <= maze[4];
+            map[map_l+5] <= maze[5];
+            map[map_l+6] <= maze[6];
+            map[map_l+7] <= maze[7];
+            map[79] <= 1'b1;
             count_maze <= count_maze+1;
             col<=3'd0;
             row<=3'd0;
             path_num <= 4'd0;
         end else begin
             map[project_p] <= 1'b0;
-            if (!complete_ans || count_rat!=15) begin
+            if (!complete_ans) begin
                 if(dead_path) begin
                     row <= stack_row;
                     col <= stack_col;
@@ -42,15 +59,16 @@ always@(posedge clk or negedge rst_n)begin
                     ans_row[path_num] <= row;
                     ans_col[path_num] <= col;
                     if(down_right)
-                        row<=row+1;
-                    else
                         col<=col+1;
+                    else
+                        row<=row+1;
                     path_num <= path_num+1;
                 end
             end 
         end
     end
 end
+wire stack_w_en;
 corner now_corner(
     .down_p(map[project_p_d]),
     .right_p(map[project_p_r]),
@@ -59,8 +77,8 @@ corner now_corner(
     .is_corner_out(stack_w_en)
 );
 stack my_stack(
-    .clk,
-    .rst_n,
+    .clk(clk),
+    .rst_n(rst_n),
     .w_en(stack_w_en),
     .pop(dead_path),
     .clr(in_valid),
@@ -69,7 +87,7 @@ stack my_stack(
     .path_num_in(path_num),
     .row_out(stack_row),
     .col_out(stack_col),
-    .path_num_out(stack_col_path_num)
+    .path_num_out(stack_path_num)
 );
 /************* input maze *************/
 
@@ -83,7 +101,7 @@ always@(posedge clk or negedge rst_n)begin
             out_col <=ans_col[count_rat];
             out_row <=ans_row[count_rat];
             count_rat <= count_rat+1;
-        end else if(in_valid)begin
+        end else begin
             count_rat<=0;
             out_col<=0;
             out_row<=0;
@@ -122,9 +140,10 @@ module stack(
     output [2:0] row_out,col_out,
     output [3:0] path_num_out
 );
-reg [3:0] count;
-reg [2:0] stack_row,stack_col [2:0];
-reg [3:0] stack_path_num [2:0];
+reg [2:0] count;
+reg [2:0] stack_row [0:6];
+reg [2:0] stack_col [0:6];
+reg [3:0] stack_path_num [0:6];
 assign row_out=stack_row[count-1];
 assign col_out=stack_col[count-1];
 assign path_num_out=stack_path_num[count-1];
